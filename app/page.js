9066590ca679b208f78f5f9f940f8c7459ccc51d
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-11 18:42:49 +0530

    Update page.js without your chat app name

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>Hello,I am Page.js</main>
   );
+}
+export const metadata = {
+  title: 'Home - Piechat',
+  description: 'Piechat helps you connect with people of your choice',
 }
