c23abdce0a0bf55249bbccb5bf5a7ac321c06e4f
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-13 15:42:26 +0530

    Update page.js under forums/[slug]

--- app/forum/[slug]/page.js
+++ app/forum/[slug]/page.js
@@ -1,16 +1,5 @@
-import { Chat, useCreateChatClient } from 'stream-chat-react';
-// your Stream app information
-const apiKey = 'dz5f4d5kzrue';
-const userId = 'shiny-wildflower-8';
-const userName = 'shiny';
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2hpbnktd2lsZGZsb3dlci04IiwiZXhwIjoxNzM2NzcyNTU4fQ.ZZ12lKScGBmM-p3ZJtdmMrIvmINmARivmCj7nrpnZq0';
+import ChatForum from '@/components/ChatForum';
 export default async function Page({ params }) {
-    const client = useCreateChatClient({
-        apiKey,
-        tokenOrProvider: userToken,
-        userData: { id: userId },
-      });
       const slug = (await params).slug;
-      if (!client) return <div>Setting up client & connection...</div>;
-      return <Chat client={client}>Chat with client is ready!</Chat>;
+      return <ChatForum/>;
 }
