47eb9ef8eca3553248968aeccad4658ed3d26074
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-13 18:42:25 +0530

    Make slug as parameter in ChatForum instead of 'custom_channel_id'

--- components/ChatForum.js
+++ components/ChatForum.js
@@ -4,14 +4,14 @@ import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, Messag
 import 'stream-chat-react/dist/css/v2/index.css';    
 const apiKey = 'aubh9kt3dvbc';
 const userId = 'user_2rTqxp7yR2aawcRCZ8LJUTIx2qT';
-const userName = 'Lavanya';
+const userName = 'SLavanya';
 const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yclRxeHA3eVIyYWF3Y1JDWjhMSlVUSXgycVQifQ._YwEJ0FstJb0Nfpu88zim4SUYApsB0miHLu809EihYU';
 const user= {
     id: userId,
     name: userName,
     image: `https://getstream.io/random_png/?name=${userName}`,
 };
-export default function ChatForum() {
+export default function ChatForum(slug) {
     const [channel, setChannel] = useState();
     const client = useCreateChatClient({
     apiKey,
@@ -20,7 +20,7 @@ export default function ChatForum() {
     });
     useEffect(() => {
         if (!client) return;
-        const channel = client.channel('messaging', 'custom_channel_id', {
+        const channel = client.channel('messaging',slug, {
           image: 'https://getstream.io/random_png/?name=react',
           name: 'Talk about React',
           members: [userId],
