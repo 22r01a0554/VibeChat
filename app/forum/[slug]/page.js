8ba5d027ff4ce9519c0b9eaeec9aac89106ef09c
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-13 15:37:34 +0530

    Call ChatForum.js

--- app/forum/[slug]/page.js
+++ app/forum/[slug]/page.js
@@ -1,4 +1,16 @@
+import { Chat, useCreateChatClient } from 'stream-chat-react';
+// your Stream app information
+const apiKey = 'dz5f4d5kzrue';
+const userId = 'shiny-wildflower-8';
+const userName = 'shiny';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2hpbnktd2lsZGZsb3dlci04IiwiZXhwIjoxNzM2NzcyNTU4fQ.ZZ12lKScGBmM-p3ZJtdmMrIvmINmARivmCj7nrpnZq0';
 export default async function Page({ params }) {
-    const slug = (await params).slug
-    return <div>My Post: {slug}</div>
+    const client = useCreateChatClient({
+        apiKey,
+        tokenOrProvider: userToken,
+        userData: { id: userId },
+      });
+      const slug = (await params).slug;
+      if (!client) return <div>Setting up client & connection...</div>;
+      return <Chat client={client}>Chat with client is ready!</Chat>;
 }
