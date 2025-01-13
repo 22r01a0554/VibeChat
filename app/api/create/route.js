7a76b8e0618e0d8a91495a093ea9ee949ababdb6
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-13 23:15:30 +0530

    Create UseToken when a new user has been created

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,10 +1,22 @@
 import { StreamChat } from "stream-chat";
 const api_key = "aubh9kt3dvbc";
 const api_secret = "ep5smgj88bt8jn22fsxvc94r5jd2szzmq4en5t9mukxpfq45yauh4vqkyzd3nbte";
-const user_id = "user_2rTqxp7yR2aawcRCZ8LJUTIx2qT";
-export async function GET() {
+//static without ngrok for only one user by giving userid before only
+// const user_id = "user_2rTqxp7yR2aawcRCZ8LJUTIx2qT";
+// export async function GET() {
+//     const serverClient = StreamChat.getInstance(api_key, api_secret);
+// //Create User Token
+//     const token = serverClient.createToken(user_id);
+//     console.log(token);
+//     return Response.json({ message: 'Hello World' })
+//   }
+//Dynamic with ngrok for any number of users by giving userid when user registers
+export async function POST(request) {
     const serverClient = StreamChat.getInstance(api_key, api_secret);
-    const token = serverClient.createToken(user_id);
-    console.log(token);
+    const user = await request.json()
+    //Create User Token
+    const token = serverClient.createToken(user.id);
+    console.log("A New User HAS BEEN CREATED.");
+    console.log("A New User is Registered with ID: "+user.id);
     return Response.json({ message: 'Hello World' })
   }
