e128edfcebdd4b304e2d956fe05d26da48a57608
22r01a0554 <22r01a0554@cmrithyderabad.edu.in>
2025-01-13 15:36:58 +0530

    Create ChatForum.js under components folder and update the code from App.tsx

---
+++ components/ChatForum.js
@@ -0,0 +1,16 @@
+"use client"
+import { Chat, useCreateChatClient } from 'stream-chat-react';
+// your Stream app information
+const apiKey = 'dz5f4d5kzrue';
+const userId = 'shiny-wildflower-8';
+const userName = 'shiny';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2hpbnktd2lsZGZsb3dlci04IiwiZXhwIjoxNzM2NzcyNTU4fQ.ZZ12lKScGBmM-p3ZJtdmMrIvmINmARivmCj7nrpnZq0';
+export default function ChatForum() {
+    const client = useCreateChatClient({
+        apiKey,
+        tokenOrProvider: userToken,
+        userData: { id: userId },
+      });
+      if (!client) return <div>Setting up client & connection...</div>;
+      return <Chat client={client}>Chat with client is ready!</Chat>;
+}
